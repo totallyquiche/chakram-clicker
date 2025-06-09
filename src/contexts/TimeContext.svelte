1bfc56326e211d873ac7055de9b6fb7b3cad58e1
totallyquiche <bndady@gmail.com>
2025-06-08 21:01:33 -0500

    create timer component

---
+++ src/contexts/TimeContext.svelte
@@ -0,0 +1,18 @@
+<script module lang="ts">
+    import type { Writable } from 'svelte/store';
+
+    export type TimeContextStore = Writable<number>;
+</script>
+
+<script lang="ts">
+    import { setContext } from 'svelte';
+    import { TimeStore } from '../stores/TimeStore.svelte';
+
+    const TimeContext: TimeContextStore = setContext('TimeStore', TimeStore);
+
+    export { TimeContext };
+
+    const { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/stores/TimeStore.svelte
@@ -0,0 +1,8 @@
+<script module lang="ts">
+    import { writable } from 'svelte/store';
+    import type { TimeContextStore } from '../contexts/TimeContext.svelte';
+
+    const TimeStore: TimeContextStore = writable(0);
+
+    export { TimeStore };
+</script>
