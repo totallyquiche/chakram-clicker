0d7a8f48a4d575a6d8cb4a4319b63f11442d1680
totallyquiche <bndady@gmail.com>
2025-06-08 23:05:00 -0500

    refactor points gained

--- src/components/Chakram.svelte
+++ src/components/Chakram.svelte
@@ -23,7 +23,7 @@
     };
 </script>
 
-<div class="relative">
+<div>
     <button class="h-24 w-24" on:click={handleClick}>
         <img
             src="/chakram.png"
@@ -33,25 +33,6 @@
         />
     </button>
     {#each pointsGainedInstances as instance (instance.id)}
-        <div class="animate-float absolute top-0 left-0">
-            <PointsGained points={instance.points} />
-        </div>
+        <PointsGained points={instance.points} />
     {/each}
 </div>
-
-<style>
-    .animate-float {
-        animation: float 1s ease-out forwards;
-    }
-
-    @keyframes float {
-        0% {
-            transform: translateY(0);
-            opacity: 1;
-        }
-        100% {
-            transform: translateY(-20px);
-            opacity: 0;
-        }
-    }
-</style>
--- src/components/PointsGained.svelte
+++ src/components/PointsGained.svelte
@@ -2,4 +2,23 @@
     const { points } = $props();
 </script>
 
-<span>𐆖 {points}</span>
+<div class="animate-float absolute top-0 left-0">
+    <span>𐆖 {points}</span>
+</div>
+
+<style>
+    .animate-float {
+        animation: float 1s ease-out forwards;
+    }
+
+    @keyframes float {
+        0% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        100% {
+            transform: translateY(-20px);
+            opacity: 0;
+        }
+    }
+</style>
