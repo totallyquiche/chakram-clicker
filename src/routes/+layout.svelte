8724afb87513d8cfccc467daa597690dea4913f7
totallyquiche <bndady@gmail.com>
2025-06-09 03:32:08 -0500

    remove debug context and store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import '../app.css';
 
-    import DebugContext from '../contexts/DebugContext.svelte';
     import TimeContext from '../contexts/TimeContext.svelte';
     import DinarContext from '../contexts/DinarContext.svelte';
     import GrowthRateContext from '../contexts/GrowthRateContext.svelte';
@@ -9,12 +8,10 @@
     let { children } = $props();
 </script>
 
-<DebugContext>
-    <TimeContext>
-        <GrowthRateContext>
-            <DinarContext>
-                {@render children()}
-            </DinarContext>
-        </GrowthRateContext>
-    </TimeContext>
-</DebugContext>
+<TimeContext>
+    <GrowthRateContext>
+        <DinarContext>
+            {@render children()}
+        </DinarContext>
+    </GrowthRateContext>
+</TimeContext>
