d4803e929e122903cd1fb2a5ed2ab903d908f682
totallyquiche <bndady@gmail.com>
2025-06-08 22:46:20 -0500

    create growth rate context and store

---
+++ src/contexts/GrowthRateContext.svelte
@@ -0,0 +1,21 @@
+<script module lang="ts">
+    import type { Writable } from 'svelte/store';
+
+    export type GrowthRateContextStore = Writable<number>;
+</script>
+
+<script lang="ts">
+    import { setContext } from 'svelte';
+    import GrowthRateStore from '../stores/GrowthRateStore';
+
+    const GrowthRateContext: GrowthRateContextStore = setContext(
+        'GrowthRateStore',
+        GrowthRateStore
+    );
+
+    export { GrowthRateContext };
+
+    const { children } = $props();
+</script>
+
+{@render children()}
