144bc681168d1ab98c908dea9ee39bf246b1b53a
totallyquiche <bndady@gmail.com>
2025-06-11 21:36:53 -0500

    displays scroll

---
+++ src/components/Scroll.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let open = false;
+</script>
+
+<div class="relative flex h-full w-full items-center">
+    <img
+        alt="The left handle to a scroll"
+        src="/scroll-left.png"
+        class="h-full max-h-42 w-auto object-contain"
+    />
+    <img alt="Scroll paper" src="/scroll-paper.png" class="h-full max-h-42 w-auto object-contain" />
+    <img
+        alt="The right handle to a scroll"
+        src="/scroll-right.png"
+        class="h-full max-h-42 w-auto object-contain"
+    />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
     import Score from '../components/Score.svelte';
     import Chakram from '../components/Chakram.svelte';
     import Salmoneus from '../components/Salmoneus.svelte';
+    import Scroll from '../components/Scroll.svelte';
     import type { DinarContextStore } from '../contexts/DinarContext.svelte';
 
     const dinars: DinarContextStore = getContext('DinarStore');
@@ -20,6 +21,9 @@
     <div class="absolute top-25 right-50 text-5xl leading-normal"><Score /></div>
     <div class="absolute top-0 -left-1/4 flex h-full w-full items-center justify-center">
         <Chakram />
+    </div>
+    <div class="absolute right-1/8 bottom-5">
+        <Scroll />
     </div>
     {#if maxDinars > 20}
         <div class="absolute -right-5 -bottom-5 max-h-60 max-w-60 scale-x-[-1] rotate-340">
