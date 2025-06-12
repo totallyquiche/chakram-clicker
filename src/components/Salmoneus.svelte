bfe05d7771e7b856c1b9f20eba6deefd04ddcc0c
totallyquiche <bndady@gmail.com>
2025-06-11 20:23:39 -0500

    show salmoneus after 20 dinars reached

--- src/components/Salmoneus.svelte
+++ src/components/Salmoneus.svelte
@@ -1,1 +1,23 @@
-<img alt="Salmoneus grinning deviously" src="/salmoneus.png" width="520" height="411" />
+<img
+    class="slide-in"
+    alt="Salmoneus grinning deviously"
+    src="/salmoneus.png"
+    width="520"
+    height="411"
+/>
+
+<style>
+    @keyframes slideIn {
+        from {
+            transform: translate(-100%, 100%);
+        }
+
+        to {
+            transform: translate(0, 0);
+        }
+    }
+
+    .slide-in {
+        animation: slideIn 1s ease-out forwards;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
+    import { getContext } from 'svelte';
     import Score from '../components/Score.svelte';
     import Chakram from '../components/Chakram.svelte';
+    import Salmoneus from '../components/Salmoneus.svelte';
+    import type { DinarContextStore } from '../contexts/DinarContext.svelte';
+
+    const dinars: DinarContextStore = getContext('DinarStore');
+
+    let maxDinars = $dinars;
+
+    dinars.subscribe((value) => {
+        if (value > maxDinars) {
+            maxDinars = value;
+        }
+    });
 </script>
 
 <div class="m-12">
@@ -8,4 +21,9 @@
     <div class="absolute top-0 -left-1/4 flex h-full w-full items-center justify-center">
         <Chakram />
     </div>
+    {#if maxDinars > 20}
+        <div class="absolute -right-5 -bottom-5 max-h-60 max-w-60 scale-x-[-1] rotate-340">
+            <Salmoneus />
+        </div>
+    {/if}
 </div>
