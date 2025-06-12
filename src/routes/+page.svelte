bb0d77ee48df408e181463779a27658058e04b68
totallyquiche <bndady@gmail.com>
2025-06-11 20:02:44 -0500

    update background image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import Score from '../components/Score.svelte';
     import Chakram from '../components/Chakram.svelte';
+    import Salmoneus from '../components/Salmoneus.svelte';
 </script>
 
 <div class="m-12">
     <div class="text-5xl leading-normal"><Score /></div>
-    <div class="absolute top-0 left-0 flex h-full w-full items-center justify-center">
+    <div class="absolute top-0 -left-1/4 flex h-full w-full items-center justify-center">
         <Chakram />
     </div>
 </div>
