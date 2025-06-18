53f3648d5a00106eb197dbe4026d85e90bd5d6f8
totallyquiche <bndady@gmail.com>
2025-06-18 17:52:05 -0500

    add vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
 import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
     kit: {
         adapter: adapter()
     },
